33d749fe93bca1fac293d233db6bad2b563e2f80
Kerlan <guillaume.feltrin@gmail.com>
2018-01-09 16:32:53 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -13,7 +13,7 @@ and open the template in the editor.
         <link href="CSS/font-awesome-4.7.0/css/font-awesome.min.css" rel="stylesheet" type="text/css"/>
     </head>
     <body class="landing">
-        <?php session_start(); ?>
+
         <!-- Header -->
         <header id="header">
             <h1><a href="index.php"><img class="img-responsive" src="Images/logoMenu2.png" alt=""/></a></h1>
@@ -27,17 +27,8 @@ and open the template in the editor.
 
             <nav>
                 <ul>
-                    <?php
-                        if (isset($_SESSION['login']) && isset($_SESSION['password'])){
-                            echo '<li><a href="membres.php"><i class="fa fa-user" style="font-size:30px;"></i></a></li>';
-                            echo '<li><a href="Logout.php"><i class="fa fa-sign-out"></i> Log out </a></li>';                         
-                        }
-                        
-                        else{
-                            echo '<li><a href="Log.php"><i class="fa fa-user-plus"></i> Sign Up </a></li>';
-                            echo '<li><a href="Log.php"><i class="fa fa-sign-in"></i> Sign In </a></li>';
-                        }
-                     ?>
+                    <li><a href="Log.php"><i class="fa fa-user"></i> Sign Up </a></li>
+                    <li><a href="Log.php"><i class="fa fa-sign-in"></i> Sign In </a></li>
                     <li><a href="Contact.php"><i class="fa fa-address-book"></i> Contact</a>
                     <li><a href="#"><i class="fa fa-shopping-cart" id="caddie"></i></a></li>
                 </ul>
@@ -48,7 +39,6 @@ and open the template in the editor.
         <!-- Banner -->
         <section id="banner">
             <img class="img-responsive" src="Images/logo.png" alt=""/>
-
             <p>Realize your children's dreams !</p>
             <br>
             <a href="#" class="btn btn-primary btn-lg">Get Your Penguin</a>
@@ -56,27 +46,43 @@ and open the template in the editor.
 
         <div class="container col-lg-12" id="one">
             <div class="row">
-                <h3>Insert your text here</h3>
-                <div class="col-lg-4">
+                <h3>Come on and get your penguin !</h3>
+                <div class="col-lg-4">                    
                     <i class="icon big rounded fa-snowflake-o"></i>
+                    <h4>Chose your favorite penguin <h4> 
+                    <h4>and put it in your shopping cart !<h4>
                 </div>
                 <div class="col-lg-4">
                     <i class="icon big rounded fa-shopping-cart"></i>
+                    <h4>Easily way to buy the penguin</h4>
+                    <h4>of your dreams !</h4>
                 </div>
                 <div class="col-lg-4">
                     <i class="icon big rounded fa-plane"></i>
+                    <h4> Get your penguin all around </h4>
+                    <h4>the world !</h4>
                 </div>
 
             </div>
         </div>
 
-        <div class="container col-lg-12" id="two">
+        <div class="container" id="two">
             <div class="row">
-                <i class="fa fa-cog fa-spin fa-3x fa-fw"></i>
-                <span class="sr-only">Loading...</span>
+                <div class="col-lg-12">
+                    <h1>Who are we ?</h1>
+                    <section class="col-lg-12" id="sectionh">
+                        <h3>We are a group of four students. We wanted to create
+                         a website where we can selling penguins.
+                        We decided to sell penguins because we love them and we want to give them a newlife.
+                         You can find your favorite penguins that make you very happy.</h3>
+                        
+                            <img src="Images/nick-karvounis-142025.jpg" alt="manchot1"/>
+                            <img src="Images/paz-arando-132661.jpg" alt="manchot2"/>
+                            <img src="Images/jay-ruzesky-91049.jpg" alt="manchot3"/>
+                    </section>
+                </div> 
             </div>
         </div>
-
 
         <footer>
             <div class="container col-lg-12">
@@ -85,6 +91,7 @@ and open the template in the editor.
                         <li><a href="https://www.facebook.com/Your-penguin-370472053409761"><i class="fa fa-facebook-square"></i></a></li>
                         <li><a href="https://twitter.com/GetPenguin6"><i class="fa fa-twitter"></i></a>
                         <li><a href="#"><i class="fa fa-instagram"></i></a>
+                        
                     </ul>
                     <p><i class="fa fa-copyright"></i> copyright. All rights reserved.</p>
                     <p><i class="fa fa-camera"></i> Unsplash</p>
